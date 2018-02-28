a4001076dcdd7881f945b92196abb2030bee0d21
Ashish Acharya <ashish.acharya14@gmail.com>
2018-02-28 16:14:27 +0545

    first pass, gets all records and saved them in a list of dictionaries

--- dcf_to_json.py
+++ dcf_to_json.py
@@ -1,1 +1,29 @@
 import json
+from pprint import pprint
+
+inRecord = False
+inItem = False
+
+records = list()
+
+with open('dictionary.dcf') as inspectionfile:
+    for line in inspectionfile:
+        if line.strip() == '[Record]':
+            inRecord = True
+            thisrecord = {}
+            continue
+
+        if inRecord:
+            if line.strip():
+                splits = line.split('=')
+                try:
+                    thisrecord[splits[0].strip()] = splits[1].strip()
+                except IndexError:
+                    print(splits)
+
+            if not line.strip():
+                # inItem = True
+                inRecord = False
+                records.append(thisrecord)
+
+pprint(records)
